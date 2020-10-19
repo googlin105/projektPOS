c4e30916044538707b1e910dc1ec28e8aac45f52
gogolin105 <jagielskijedrek@gmail.com>
2020-10-19 15:55:52 +0200

    Dodanie klas dziedziczacych Vehicle i Ship

--- projektPOS.cpp
+++ projektPOS.cpp
@@ -3,6 +3,8 @@
 #include <time.h>
 
 
+
+
 class Map {
 private:
     int length;
@@ -30,7 +32,11 @@ private:
     int static numOfPoints;
     int x;
     int y;
+protected:
+    int mapL;                                 //zamieszczenie informacji o wielkosci mapy w klasie Point
+    int mapH;
 public:
+
     int giveX() { return x; }
     int giveY() { return y; }
     void setPoint(int x, int y) {
@@ -42,8 +48,10 @@ public:
     Point(int l, int h) { 
         this->id = Point::numOfPoints+1;
         Point::numOfPoints+=1; 
-        this->x = rand() % l;
+        this->x = rand() % l;                 //losowa generacja punktu poczatkowego
         this->y = rand() % h;
+        this->mapL = l;
+        this->mapH = h;
     }
     Point() {
         this->id = Point::numOfPoints + 1;
@@ -54,6 +62,36 @@ public:
 
 };
 
+class Vehicle : public Point {
+private:
+    int speed;
+public:
+    Vehicle(int l, int h, int speed) : Point(l, h) {
+            this->speed = speed;
+    }
+    int giveSpeed() { return this->speed; }
+    
+};
+
+class Ship : public Vehicle {
+private:
+    int destX;
+    int destY;
+public:
+    Ship(int l, int h, int speed) :Vehicle(l, h, speed) {
+        this->destX = rand() % this->mapL;            // wyznaczenie punktu koncowego
+        this->destY = rand() % this->mapH;
+    };
+    int giveDestX() { return this->destX; }
+    int giveDestY() { return this->destY; }
+    void run() {
+        
+    }
+
+
+};
+
+
 int Point::numOfPoints = 0;
 
 void Map::toString() {
@@ -63,13 +101,15 @@ void Map::toString() {
 int main()
 {
     srand(time(NULL));
-    Map mapa = Map(100,400);
+    const int LEN = 200;
+    const int HEI = 200;
+    Map mapa = Map(LEN,HEI);
     mapa.toString();
-    Point p1 = Point(mapa.giveL(), mapa.giveH());
-    Point p2 = Point(mapa.giveL(), mapa.giveH());
+    Ship v1 = Ship(mapa.giveL(), mapa.giveH(),20);
+    Ship v2 = Ship(mapa.giveL(), mapa.giveH(),10);
     
-    //std::cout << Point::giveNumOfPoints()<<"\n\n";
-    printf("Punkt %d - %dx%d\n", p1.giveID(), p1.giveX(), p1.giveY());
+    printf("Punkt %d - %dx%d; %d; dest - %dx%d\n", v1.giveID(), v1.giveX(), v1.giveY(),v1.giveSpeed(),v1.giveDestX(),v1.giveDestY());
+    printf("Punkt %d - %dx%d; %d; dest - %dx%d\n", v2.giveID(), v2.giveX(), v2.giveY(), v2.giveSpeed(), v2.giveDestX(), v2.giveDestY());
     
 
 }
