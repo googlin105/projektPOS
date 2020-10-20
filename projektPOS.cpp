7fbba37ffde36d545bd7f4bc991f5ff0fe948484
gogolin105 <jagielskijedrek@gmail.com>
2020-10-20 18:37:23 +0200

    Zmiana vehicle i ship - dodanie funkcji walkLine Zmiana pol private, public i protected poszczegolnych klas

--- projektPOS.cpp
+++ projektPOS.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <thread>
+#include <chrono>
+
 
 
 
@@ -30,9 +33,10 @@ class Point {
 private:
     int id;
     int static numOfPoints;
+
+protected:
     int x;
     int y;
-protected:
     int mapL;                                 //zamieszczenie informacji o wielkosci mapy w klasie Point
     int mapH;
 public:
@@ -63,29 +67,75 @@ public:
 };
 
 class Vehicle : public Point {
-private:
+protected:
     int speed;
+    int destX;
+    int destY;
+    void walkLine(int start, int end, char ch) {
+        int diff = start - end;
+        bool flag;
+        if (diff >= 0) {
+            flag = true;
+        }
+        else {
+            diff = -diff;
+            flag = false;
+        }
+        int hops = diff / this->speed;
+        int rest = diff % this->speed;
+        while (hops > 0) {
+            if (flag) {
+                if (ch == 'X') { this->x -= speed; }
+                if (ch == 'Y') { this->y -= speed; }
+
+            }
+            else {
+                if (ch == 'X') { this->x += speed; }
+                if (ch == 'Y') { this->y += speed; }
+            }
+            hops--;
+        }
+        if (flag) {
+            if (ch == 'X') { this->x -= rest; }
+            if (ch == 'Y') { this->y -= rest; }
+
+        }
+        else {
+            if (ch == 'X') { this->x += rest; }
+            if (ch == 'Y') { this->y += rest; }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        printf("%dx%x\n", this->giveX(), this->giveY());
+
+    }
 public:
     Vehicle(int l, int h, int speed) : Point(l, h) {
             this->speed = speed;
+            this->destX = rand() % this->mapL;            // wyznaczenie punktu koncowego
+            this->destY = rand() % this->mapH;
     }
+    int giveDestX() { return this->destX; }
+    int giveDestY() { return this->destY; }
     int giveSpeed() { return this->speed; }
+    
     
 };
 
 class Ship : public Vehicle {
 private:
-    int destX;
-    int destY;
+   
+    
 public:
-    Ship(int l, int h, int speed) :Vehicle(l, h, speed) {
-        this->destX = rand() % this->mapL;            // wyznaczenie punktu koncowego
-        this->destY = rand() % this->mapH;
-    };
-    int giveDestX() { return this->destX; }
-    int giveDestY() { return this->destY; }
+    Ship(int l, int h, int speed) :Vehicle(l, h, speed) {};
     void run() {
-        
+        int startX = this->x;
+        int startY = this->y;
+        while (true) {
+            this->walkLine(this->x, this->destX, 'X');
+            this->walkLine(this->y, this->destY, 'Y');
+            this->walkLine(this->x, startX, 'X');
+            this->walkLine(this->y, startY, 'Y');
+        }      
     }
 
 
@@ -105,11 +155,13 @@ int main()
     const int HEI = 200;
     Map mapa = Map(LEN,HEI);
     mapa.toString();
-    Ship v1 = Ship(mapa.giveL(), mapa.giveH(),20);
-    Ship v2 = Ship(mapa.giveL(), mapa.giveH(),10);
+    Ship v1 = Ship(mapa.giveL(), mapa.giveH(),10);
+    Ship v2 = Ship(mapa.giveL(), mapa.giveH(),5);
     
     printf("Punkt %d - %dx%d; %d; dest - %dx%d\n", v1.giveID(), v1.giveX(), v1.giveY(),v1.giveSpeed(),v1.giveDestX(),v1.giveDestY());
     printf("Punkt %d - %dx%d; %d; dest - %dx%d\n", v2.giveID(), v2.giveX(), v2.giveY(), v2.giveSpeed(), v2.giveDestX(), v2.giveDestY());
+    v1.run();
+
     
 
 }
